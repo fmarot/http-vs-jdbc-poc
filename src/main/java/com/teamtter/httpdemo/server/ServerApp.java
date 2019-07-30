a80621de8db73d13f488f809c662a3befc761157
francois.marot <francois.marot@olea-medical.com>
2019-07-30 12:02:12 +0200

    migrated to same version of H2 as Olea

--- src/main/java/com/teamtter/httpdemo/server/ServerApp.java
+++ src/main/java/com/teamtter/httpdemo/server/ServerApp.java
@@ -2,18 +2,12 @@ package com.teamtter.httpdemo.server;
 
 import java.sql.SQLException;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.h2.tools.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -31,7 +25,14 @@ public class ServerApp {
 	/** Start internal H2 server so we can query the DB from IDE */
     @Bean(initMethod = "start", destroyMethod = "stop")
     public Server h2Server() throws SQLException {
-    	// will listen on all addresses
+    
+    	// Create SEPERATE LOBs!!!
+		System.setProperty("h2.lobInDatabase", "false");//$NON-NLS-1$ //$NON-NLS-2$
+		// 3 seconds max (in stead of 200 ms by default) in order to execute the SHUTDOWN COMPACT as done by h2 database 
+		// when it is closed
+		System.setProperty("h2.maxCompactTime", "3000");//$NON-NLS-1$ //$NON-NLS-2$
+    			
+		// will listen on all addresses
         return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-webAllowOthers", "-tcpPort", serverJdbcPort);
     }
 /*
